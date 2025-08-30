a083d40b416f0e23be28cda24f21ea819340f18b
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-30 15:31:49 +0200

    1

--- types/response.ts
+++ types/response.ts
@@ -1,11 +1,5 @@
 import type { User } from './user';
 
-export type AuthResponse = {
-  user: User;
-  accessToken: string;
-  refreshToken: string;
-};
-
 export type SessionResponse = {
   authenticated: boolean;
   user: User | null;
