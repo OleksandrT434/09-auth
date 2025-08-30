7ba93cb0a131babb367a88853ba66361df45500f
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-30 13:57:01 +0200

    4

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,12 +1,6 @@
 import axios from 'axios';
 
-const API_URL = (process.env.NEXT_PUBLIC_API_URL || '').trim().replace(/\/+$/, '');
-
-if (!API_URL) {
-  throw new Error('Set NEXT_PUBLIC_API_URL (e.g. https://notehub-api.goit.study)');
-}
-
 export const nextServer = axios.create({
-  baseURL: API_URL, 
+  baseURL : process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
 });
