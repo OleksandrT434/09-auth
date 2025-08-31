d567b95844176b7496878ea9f28c5f6433a01d1f
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-31 10:39:41 +0200

    e2

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  (process.env.NEXT_PUBLIC_API_URL) + "/api";
+
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+  baseURL,
   withCredentials: true,
 });
+
+export default nextServer;
