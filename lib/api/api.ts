8cfc936dbb7d549f832f3acfe39ff528b71b0bac
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-31 12:22:44 +0200

    12

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
+
+export default function getBaseUrl(): string {
+  if (process.env.SITE_URL) {
+    return process.env.SITE_URL;
+  }
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  if (process.env.NEXT_PUBLIC_API_URL) {
+    return process.env.NEXT_PUBLIC_API_URL;
+  }
+  throw new Error('Base URL is not defined in environment variables');
+}
 
 export const nextServer = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
-    headers: { "Content-Type": "application/json" },
-    withCredentials: true,
+  baseURL : process.env.NEXT_PUBLIC_API_URL + '/api',
+  withCredentials: true,
 });
